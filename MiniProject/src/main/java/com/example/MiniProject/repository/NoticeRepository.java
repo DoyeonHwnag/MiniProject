f11b2b0bf8cd5a451665740add4f175a5ce5c595
DoyeonHwang <hersmil12@gmail.com>
2025-05-16 13:20:05 +0900

    Delete MiniProject directory

--- MiniProject/src/main/java/com/example/MiniProject/repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.MiniProject.repository;
-
-import com.example.MiniProject.entity.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
